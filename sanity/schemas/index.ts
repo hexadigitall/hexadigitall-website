898a32b290ad9e05cb8a32a47402e421daf19532
Hexadigitall <hexadigitztech@gmail.com>
2025-09-03 10:45:31 +0100

    fix: move sanity folder into src directory

--- sanity/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-// sanity/schemas/index.ts
-import service from './service';
-import project from './project';
-import post from './post';
-import faq from './faq';
-import testimonial from './testimonial';
-
-export const schemaTypes = [service, project, post, faq, testimonial];
---
+++ src/sanity/schemas/index.ts
@@ -0,0 +1,8 @@
+// sanity/schemas/index.ts
+import service from './service';
+import project from './project';
+import post from './post';
+import faq from './faq';
+import testimonial from './testimonial';
+
+export const schemaTypes = [service, project, post, faq, testimonial];
