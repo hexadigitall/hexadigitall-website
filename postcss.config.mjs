3bcb84290e43faa1e2beffb4b07d18ee92295aec
Hexadigitall <hexadigitztech@gmail.com>
2025-09-03 07:20:17 +0100

    fix: update tailwindcss postcss configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    // This is the updated way to include Tailwind
+    'tailwindcss/nesting': {}, 
     tailwindcss: {},
     autoprefixer: {},
   },
